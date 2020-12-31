d7ea37fcfa50c151d789f0824797e54fd93c66ff
yangjingyu <1193141827@qq.com>
2020-12-31 10:11:50 +0800

    Using timer, transparent full screen window and canvas, the elements can follow the mouse

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WindowsDesktopEffects
 {
@@ -23,6 +14,19 @@ namespace WindowsDesktopEffects
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();//创建timer定时器
+            timer.Interval = TimeSpan.FromMilliseconds(50);//设置周期为1000毫秒
+            timer.Tick += new EventHandler(Refresh_Time);
+            timer.Start();
+        }
+        public void Refresh_Time(object sender, EventArgs e)
+        {
+            //string time = DateTime.Now.ToString("hh:mm:ss");//获取系统时间
+            var p = System.Windows.Forms.Control.MousePosition;
+            Canvas.SetLeft(ellipse, p.X + 30);
+            Canvas.SetTop(ellipse, p.Y + 30);
+            Debug.WriteLine("{0},{1}", p.X, p.Y);
         }
     }
 }
